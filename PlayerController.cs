fbe7c5afd398bf4db492c8366f264a566f1ae278
Marco <marco.octavio.luna@gmail.com>
2021-02-17 09:19:09 -0400

    Commit 0, se agrego el script para controlar al player, se llama PlayerController

---
+++ PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //VARIABLES//    
+    //
+    private bool isGround  = true;
+    //Esto es para la fuerza con la que el player va a saltar
+    public float JumpForce = 20f;
+    //Estamos haciendo referencia al RigidBody2D del player
+    private Rigidbody2D Rigidbody;
+
+
+
+
+    private void Awake() {
+        Rigidbody = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.Space)){
+            Rigidbody.AddForce(Vector2.up * JumpForce);
+        }
+                
+    }
+
+
+    void JumpController(){
+
+    }
+}
