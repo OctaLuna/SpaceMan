6795374ffa05f71bd562f2f9e321b061ee93015d
Marco <marco.octavio.luna@gmail.com>
2021-02-18 11:34:10 -0400

    Commit 1, en este commit se programo una forma de saltar, hay otra en el siguiente commit, se llama PlayerController  Changes to be committed:

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,31 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //VARIABLES//    
-    //
-    private bool isGround  = true;
+
+    private bool isGround = false;
+    /////////Variables para el salto del personaje
     //Esto es para la fuerza con la que el player va a saltar
-    public float JumpForce = 20f;
+    public float JumpForce;
     //Estamos haciendo referencia al RigidBody2D del player
     private Rigidbody2D Rigidbody;
+    //Esta variable es para saber quien es el suelo
+    public LayerMask groundMask; 
 
 
-
-
+    //Es un void que se inicia cuando el juego inicia 
     private void Awake() {
         Rigidbody = GetComponent<Rigidbody2D>();    
     }
 
     void Update()
     {
-        
-        if(Input.GetKey(KeyCode.Space)){
-            Rigidbody.AddForce(Vector2.up * JumpForce);
+        if(isGround == true){
+            //Esto es para que cuando apretemos la tecla de space se active la funcion
+            if(Input.GetKeyDown(KeyCode.Space)){
+                //Estamos llamando a la funcion creada 
+                JumpController();
+                isGround = false;
+                Debug.Log("Salto");
+            }
         }
                 
     }
 
 
+    //Salto
     void JumpController(){
-
+        //Esto es para que el player salte
+        Rigidbody.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
     }
+
+    //Esto es para ver si el player esta tocando el suelo o no
+    bool isTouchingTheGround(){
+        return Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundMask);
+        //if(Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundMask)){
+                             
+        //}
+        //else{
+
+        //}
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Ground")){
+            isGround = true;
+        }
+    }
+
 }
